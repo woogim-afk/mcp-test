7f8de68cb98da3b6036b5f2658697c23614ad8e4
woogim-afk <woogim@gmail.com>
2026-04-25 14:55:16 +0900

    mcp test

---
+++ Desktop/mcp_test/server.py
@@ -0,0 +1,11 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("add-mcp-http")
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="0.0.0.0", port=8000)
